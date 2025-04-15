e61694143041706ff1af2cb879d80ca26246701a
Bykadorov Mikhail <mikhailbykadorov@mail.ru>
2025-04-15 15:11:11 +0300

    Updated java project
    Rename class Desk to Board

---
+++ src/Board.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+/**
+ * Доска.
+ */
+public class Board {
+
+    /**
+     * Список ячеек.
+     */
+    private List<Cell> cells;
+
+    /**
+     * Клон доски.
+     */
+    private Board clonedBoard;
+
+    /**
+     * Получить клон доски.
+     * @return клон доски.
+     */
+    public Board getClone() {
+        return this.clonedBoard; }
+
+    /**
+     * Получить список ячеек.
+     * @return список ячеек
+     */
+    public List<Cell> getCells() {
+        return cells;
+    }
+
+    /**
+     * Клонировать доску.
+     */
+    public void cloneDesk(){
+        this.clonedBoard = this;
+    }
+
+}
--- src/Desk.java
+++
@@ -1,6 +0,0 @@
-
-/**
- * Доска.
- */
-public class Desk {
-}
